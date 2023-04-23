2d6a380a65737d91e0031efc81610d928cedc51c
VladBardin <vladyslav.bardin@gmail.com>
2023-04-23 11:46:22 +0300

    First commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import database.DatabaseQueryService;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println(new DatabaseQueryService().findMaxSalary().toString());
+        System.out.println(new DatabaseQueryService().findMaxProjectCountClient().toString());
+        System.out.println(new DatabaseQueryService().findLongestPrs().toString());
+        System.out.println(new DatabaseQueryService().findYoungOldWorker().toString());
+        System.out.println(new DatabaseQueryService().printPrPrice().toString());
+
+    }
+}
